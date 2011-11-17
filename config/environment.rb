f567b5b63a5e92a10d96db4ad3ba7b63a4454c6f
Kangin <lucas@lucas-laptop.(none)>
2011-11-17 18:18:43 +0100

    Changement environement.rb avec hassle

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
-# Load the rails application
+e# Load the rails application
 require File.expand_path('../application', __FILE__)
-
+require 'hassle'
 # Initialize the rails application
-DemoApp::Application.initialize!
+DemoApp::Application.run do |config|
+  config.middleware.use Hassle
+end
